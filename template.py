5aa850bcf0871d8a9752fcc075684465bf415ac9
troeshust <troeshust96@gmail.com>
2025-02-17 04:43:37 +0300

    fix

--- template.py
+++ template.py
@@ -6,14 +6,13 @@ import random
 RANDOM = random.getrandbits(32)
 LOCAL_OUTPUT = 'output.txt'
 LOCAL_INPUT = 'input.txt'
-
-input = sys.stdin.buffer.readline # for numerican inputs
-# input=lambda: sys.stdin.readline().rstrip("\r\n") # for text inputs
-
 if(path.exists(LOCAL_OUTPUT)):
     sys.stdout = open(LOCAL_OUTPUT,'w')
 if(path.exists(LOCAL_INPUT)):
     sys.stdin = open(LOCAL_INPUT,'r')
+
+input = sys.stdin.buffer.readline # for numerican inputs
+# input=lambda: sys.stdin.readline().rstrip("\r\n") # for text inputs
 
 
 def main():
